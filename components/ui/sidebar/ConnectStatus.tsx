c03ae976a1e391a884844583ec43ad59856cdc4a
h4sh3d <h4sh3d@protonmail.com>
2023-01-04 11:36:15 +0100

    feat: display notification on connection change
    User will receive a notification when connected or disconnected to the
    node.

--- components/ui/sidebar/ConnectStatus.tsx
+++ components/ui/sidebar/ConnectStatus.tsx
@@ -1,7 +1,11 @@
 'use client'
 
 import { cva } from 'class-variance-authority'
+import { useEffect } from 'react'
+import { toast } from 'react-toastify'
 import { useConnected } from '../../../app/hooks'
+
+export type ConnectionStatus = 'connecting...' | 'connected' | 'disconnected'
 
 const connectionLabel = cva(['rounded-full', 'py-1', 'px-4', 'ring-1'], {
   variants: {
@@ -22,7 +26,18 @@ export default function ConnectStatus() {
   const connecting = 'connecting...'
   const connect = 'connected'
   const disconnected = 'disconnected'
-  const status = connected === null ? connecting : connected ? connect : disconnected
+  const status: ConnectionStatus = connected === null ? connecting : connected ? connect : disconnected
+
+  useEffect(() => {
+    switch (status) {
+      case 'connected':
+        toast.info('You are now connected to your node!')
+        break
+      case 'disconnected':
+        toast.error('Your node is disconnected!')
+        break
+    }
+  }, [status])
 
   return (
     <div className="text-center">
