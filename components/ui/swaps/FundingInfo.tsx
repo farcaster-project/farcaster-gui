8c76f35cf9b34388017d314ec6ff7ff080f79ce9
h4sh3d <h4sh3d@protonmail.com>
2023-01-06 15:38:07 +0100

    feat: add icon in needs funding info

--- components/ui/swaps/FundingInfo.tsx
+++ components/ui/swaps/FundingInfo.tsx
@@ -1,10 +1,16 @@
 import { FundingItem } from '../../lists/RunningList'
 import { formatAmount } from '../../utils'
+import { GiCash } from 'react-icons/gi'
 
 export function FundingInfo({ info }: { info: FundingItem }) {
   return (
     <div className="p-4 mt-6 mb-12 bg-orange-100 text-orange-900 rounded-lg">
-      <div className="font-bold text-xl text-orange-800 mb-2">This swap needs funding!</div>
+      <div className="font-bold text-xl text-orange-800 mb-2 flex space-x-2 items-center">
+        <div>
+          <GiCash />
+        </div>
+        <div>This swap needs funding!</div>
+      </div>
       <div className="flex space-x-3 items-center">
         <span>Send</span>
         <span className="font-mono py-1 px-3 bg-orange-200 rounded">
